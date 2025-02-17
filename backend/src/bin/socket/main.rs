5d681078e1797f6dabef58b5cb1880225ad57496
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2025-02-17 23:29:47 +0000

    message struct

--- backend/src/bin/socket/main.rs
+++ backend/src/bin/socket/main.rs
@@ -1,11 +1,58 @@
-use std::net::{TcpListener, TcpStream, Shutdown};
+use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::cmp;
+use std::vec::Vec;
 
 enum Command {
-    Save,
+    Update,
     Query,
+
+    Error,
     Unknown,
+}
+struct Message {
+    pub command: u8,
+    pub clock:   u32,
+    pub length:  u32,
+    pub message: Vec<u8>,
+}
+impl Message {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+        buffer.push(self.command);
+        buffer.extend_from_slice(&self.clock.to_be_bytes());
+
+        buffer.extend_from_slice(&self.length.to_be_bytes());
+        buffer.extend_from_slice(&self.message);
+
+        buffer
+    }
+
+    fn header_from_bytes(data: &[u8]) -> Result<Self, String> {
+        if data.len() < 9 {
+            return Err("Data too short to be a valid message".to_string());
+        }
+        let command = data[0];
+
+        let clock = u32::from_be_bytes(data[1..5].try_into().unwrap());
+        let length = u32::from_be_bytes(data[5..9].try_into().unwrap());
+
+        Ok(Self{
+            command,
+            clock,
+            length,
+            message: vec![],
+        })
+    }
+
+    fn get_command(&self) -> Command {
+        match  self.command {
+            0 => Command::Save,
+            1 => Command::Query,
+            _ => Command::Unknown,
+            
+        }
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -18,31 +65,33 @@ fn handle_connection(mut stream: TcpStream) {
             break;
         }
 
-        let command_code = header[0];
-        let command = match command_code {
-            0 => Command::Save,
-            1 => Command::Query,
-            _ => Command::Unknown,
-            
-        };
-        let msg_clock = u32::from_be_bytes(header[1..5].try_into().unwrap());
-        clock = cmp::max(msg_clock,clock);
+        let mut msg = Message::header_from_bytes(&header).unwrap();
+        clock = cmp::max(clock, msg.clock);
 
-        let msg_length = u32::from_be_bytes(header[5..9].try_into().unwrap());
-
-        let mut message = vec![0; msg_length as usize];
-        if stream.read_exact(&mut message).is_err() {
+        let mut buffer = vec![0;  msg.length as usize];
+        if stream.read_exact(&mut buffer).is_err() {
             println!("Failed to read message.");
             break;
         }
-        handle_message(command, message);
+        msg.message = buffer;
+        handle_message(msg);
         
         stream.write_all(b"Hello, Client!").unwrap();
         todo!("build and send return message");
     }
 }
 
-fn handle_message(cmd: Command, message: Vec<u8>) {}
+fn handle_message(message: Message) -> Message {
+    match message.get_command() {
+        Command::Update => {
+            todo!("make update to dag");
+        }
+        Command::Query => {
+        }
+        Command::Unknown => {
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()>  {
