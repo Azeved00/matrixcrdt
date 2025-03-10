44b1554f09957b30000afcc2e08c79d390b8588b
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2025-03-10 17:13:08 +0000

    added benchmark number 2, updates and queries

---
+++ benchmarks/bench2.sh
@@ -0,0 +1,42 @@
+SAMPLE=3
+APPLY_N=5
+LOGS="./logs"
+
+# start  backend
+cargo run --manifest-path ./backend/Cargo.toml --bin socket &
+while ! nc -z localhost 20076; do
+    sleep 0.1
+done
+
+# start  frontend 1
+npm --prefix ./frontend start 3000 &
+while ! nc -z localhost 3000; do
+    sleep 0.1
+done
+
+# start  frontend 2
+npm --prefix ./frontend start 3001 &
+while ! nc -z localhost 3001; do
+    sleep 0.1
+done
+
+
+# send curl requests to frontend
+for i in $(seq 1 $SAMPLE); do
+    for i in $(seq 1 $APPLY_N); do
+        curl --location 'localhost:3000/map' \
+            -o /dev/null  -s \
+            --header 'Content-Type: application/json' \
+            --data "{\"key\":\"123\",\"value\":\"$i\"}"
+
+        curl --location 'localhost:3000/save'  -o /dev/null  -s
+    done
+
+    curl --location 'localhost:3001/query'  -o /dev/null  -s
+done
+
+jobs -p | xargs kill
+
+# put logs in place
+# take every file and put it at /logs folder
+mv ./*.csv ./logs/
