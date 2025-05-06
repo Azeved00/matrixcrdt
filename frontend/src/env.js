ff6c794ce6778ce4e021cb9bd15da4add7e36824
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2025-05-06 10:33:38 +0100

    frontend: and env manager file (not done yet)

---
+++ frontend/src/env.js
@@ -0,0 +1,5 @@
+
+export const dev        = process.env.NODE_ENV   === 'debug';
+export const log        = process.env.NODE_ENV   === 'bench';
+
+export const state      = process.env.STATE_ENV === '';
